46fee13e6db7777c1fe58eb10bdfa89ef5afb276
PedroCarrasco82 <contatopedrocarrasco@gmail.com>
2020-04-28 22:28:01 -0300

    incluido criptografia na senha

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,9 +1,17 @@
 const Usuario = require('../models/Usuario');
+const bcrypt = require('bcrypt');
 
 module.exports = {
     async store(req,res){
         const {name,email,password,birthday_date} = req.body;
-        const {id} = await Usuario.create({name,email,password,birthday_date});
+
+        const usuario = await Usuario.findOne({where:{email}});
+        if(usuario)
+            return res.json('Esse email já está cadastrado!');
+
+        const salt = bcrypt.genSaltSync(10);
+        const passwordFinal = bcrypt.hashSync(password, salt)
+        const {id} = await Usuario.create({name,email,password:passwordFinal,birthday_date});
         return res.json(`Id is ${id}`);
     }
 }
